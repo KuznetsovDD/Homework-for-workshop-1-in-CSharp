a2f1105aacfe02327e288f47b5365015ca3914b7
Atlant <cuz.den30@gmail.com>
2022-11-17 18:51:17 +0300

    задание 4

---
+++ Задание 4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите чесло 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чесло 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чесло 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a==b&b==c)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+int max = a;
+if( max<b )
+    max=b;
+if( max<c )
+    max=c;
+
+Console.WriteLine("max" + " " + max);
+}
