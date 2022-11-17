5dda93ae09ae48c0091d584fd387308b6228ad4a
Atlant <cuz.den30@gmail.com>
2022-11-17 20:28:58 +0300

    задание 8.1.01

---
+++ Задание 8/Задание 8.01/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if(a<=0)
+{
+   while(i>a-1)
+{
+    if (i%2==0)
+    Console.Write(i + " ");
+    i =i-1; 
+}}
+else{
+while(i<a+1)
+{
+    if (i%2==0)
+    Console.Write(i + " ");
+    i ++;
+}
+}
