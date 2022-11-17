eef337b896cb1e2b27241e210e883278c40edfc3
Atlant <cuz.den30@gmail.com>
2022-11-17 19:54:51 +0300

    задание 8

---
+++ Задание 8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a<0)
+{
+    Console.WriteLine("Неверное число");
+}
+else
+{
+int i = 1;
+while(i<a+1)
+{
+    if (i%2==0)
+    Console.Write(i + " ");
+    i ++;
+}
+}
