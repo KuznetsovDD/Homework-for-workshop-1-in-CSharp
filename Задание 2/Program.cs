fbabd7565abe6e4637b8b14001cecb18f78a80e8
Atlant <cuz.den30@gmail.com>
2022-11-17 18:30:50 +0300

    Задание 2

---
+++ Задание 2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Вdедите чесло 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Вdедите чесло 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a==b)
+{
+    Console.WriteLine("Числа равны");
+
+}
+else
+if(a<b)
+{
+    Console.WriteLine("max" +  " " + b);
+    Console.WriteLine("min" +  " " + a);
+}
+else
+{
+    Console.WriteLine("max" +  " " + a);
+    Console.WriteLine("min" +  " " + b);
+}
